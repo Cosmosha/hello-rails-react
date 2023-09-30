87990936a6ed0ea92ca85b1a685db07bf9599364
cosmosha <cosmoshagan@gmail.com>
2023-09-30 08:49:03 +0000

    Add react components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greetings from "./Greetings";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Rails-React App</h2>
+        <Greetings/>
+    </div>
+  );
+}
+
+export default App;
