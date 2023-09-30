e3c04b8fc661e61aa268e94577d5ef758bd46963
cosmosha <cosmoshagan@gmail.com>
2023-09-30 09:04:10 +0000

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,15 @@
-// Entry point for the build script in your package.json
-// import "@hotwired/turbo-rails"
-// import "./controllers"
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './components/App.js';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
+    </Provider>
 );
